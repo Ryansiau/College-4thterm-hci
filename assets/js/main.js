2dc22a3b89e3e83e10f8c6ddc0a13cb95ecc7d9b
i-type-stuff <54173009+i-type-stuff@users.noreply.github.com>
2020-06-11 21:56:48 +0800

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,70 @@
+
+$(document).ready(function() {
+	$(".slider__button--next").on("click", function() {
+		var currentImg = $(".slider__image--active");
+		var nextImg = currentImg.next();
+
+		if(nextImg.length) {
+			currentImg.removeClass("slider__image--active").css("z-index", -10);
+			nextImg.addClass("slider__image--active").css("z-index", 10);
+		}
+	});
+
+	$(".slider__button--prev").on("click", function() {
+		var currentImg = $(".slider__image--active");
+		var prevImg = currentImg.prev();
+
+		if(prevImg.length) {
+			currentImg.removeClass("slider__image--active").css("z-index", -10);
+			prevImg.addClass("slider__image--active").css("z-index", 10);
+		}
+	});
+
+	$(".galleries__button").click(function() {
+		$(".galleries__big-image").attr("src", $(this).children().attr("src"));
+	});
+
+	$("#pre-register-form").on("submit", function() {
+		var email = $("#email").val();
+		var username = $("#username").val();
+		var password = $("#password").val();
+		var dob = $("#dob").val();
+
+		if(email === "") {
+			alert("Email cannot be empty!");
+			return false;
+		}
+
+		if(!email.includes("@") || !email.includes(".")) {
+			alert("Please enter a valid email!");
+			return false;
+		}
+
+		if(username.length < 3) {
+			alert("Username must contain at least 3 characters!");
+			return false;
+		}
+
+		if(password.length < 8) {
+			alert("Password must contain at least 8 characters!");
+			return false;
+		}
+
+		if($("#male").is(":not(:checked)") && $("#female").is(":not(:checked)")) {
+			alert("Gender cannot be empty!");
+			return false;
+		}
+
+		if(dob === "") {
+			alert("Date of Birth cannot be empty!");
+			return false;
+		}
+
+		if($("#tos").is(":not(:checked)")) {
+			alert("You must agree to the Terms and Conditions!");
+			return false;
+		}
+
+		return true;
+	});
+});
